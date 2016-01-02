8bac7399eb5b46ba766c18a086ef120bd86a7d59
tafulop <tamas.flp@gmail.com>
2016-01-02 12:30:53 +0100

    display and calculation separated

--- exponential_kozelites.m
+++ exponential_kozelites.m
@@ -1,7 +1,7 @@
 % variables
 k = 5;
-intervals = 2;
-power = 2;  
+intervals = 10;
+power = 2;
 
 % interval
 i_min = 0;
@@ -25,9 +25,6 @@ density = 0.0001;
     increment = length(x) / intervals;
     polynoms = zeros(intervals, power + 1);
     
-
-    figure
-    
     for i = 0 : intervals - 1
         
         % get the required interval
@@ -40,26 +37,44 @@ density = 0.0001;
         
         % create fitting curve for the generated function
         polynoms(i+1,:) = polyfit(temp_x, temp_y, 2);
-        
-        % plot
-        %y_fit = polyval(polynoms(i+1), temp_x);
-        y_fit = polynoms(i+1,1) * temp_x.^2 + polynoms(i+1,2) * temp_x + polynoms(i+1,3);
-        plot(temp_x, y_fit, 'red');
-        hold on
-        
+ 
     end
     
-    
+
 % -------------------------------------
 % plot original
 % -------------------------------------
-figure
-plot(x,y)
-title(['Exponencialis fuggveny K = ', num2str(k), ' sulytenyezovel'])
+    figure
+    plot(x,y,'linewidth',2);
+    title(['Exponencialis fuggveny K = ', num2str(k), ' sulytenyezovel'])
 
-% figure title
-set(gcf,'name','Exponential function','numbertitle','off')
+    % figure title
+    set(gcf,'name','Exponential function','numbertitle','off')
 
+% -------------------------------------
+% plot fitting curve
+% -------------------------------------
+    
+    figure
+    % figure title
+    set(gcf,'name','Fitting curve','numbertitle','off')
 
-
-
+    increment = length(x) / intervals;
+    
+    for i = 0 : intervals - 1
+        
+        % get the required interval
+        i_start = floor(increment * i) + 1;
+        i_stop = floor(increment * (i + 1));
+        
+        % create temporary function
+        temp_x = x(i_start:i_stop);
+        
+        % plot
+        y_fit = polynoms(i+1,1) * temp_x.^2 + polynoms(i+1,2) * temp_x + polynoms(i+1,3);
+        plot(temp_x, y_fit, 'red','linewidth',2);
+        hold on
+  
+    end
+     
+    title(['Kozelites n = ', num2str(intervals), ' darab masodfoku fuggvennyel'])
